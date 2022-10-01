046b3f91015e957fd0f104e38aa393b2fb0dffcf
KyawRP <22050023@myrp.edu.sg>
2022-10-02 00:21:46 +0800

    Adding Initial Files

---
+++ src/main/java/com/cwf/web/cicd/maven/App.java
@@ -0,0 +1,13 @@
+package com.cwf.web.cicd.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
